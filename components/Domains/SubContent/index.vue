200d3945ec21457c3f8558cb0e6358a7758fdc7b
Hiroya Akita <akky.develop@gmail.com>
2021-03-14 20:46:47 +0900

    fix: add main implementation

--- components/Domains/SubContent/index.vue
+++ components/Domains/SubContent/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <p>サブコンテンツです</p>
-    <div>
-      <p class="pt-5 en-title-font">aaaaa</p>
-      <p class="pt-5 en-regular-font">bbbb</p>
-      <p class="pt-5 jp-title-font">ああああ</p>
-      <p class="pt-5 jp-regular-font">日本語です</p>
+  <div class="flex flex-col mt-32 sub-content__bg">
+    <div class="flex content-center justify-center">
+      <p>Latest Information</p>
+    </div>
+
+    <div class="flex content-center justify-center">
+      <div class="w-4/6">
+        <div
+          class="grid items-center content-center grid-cols-2 justify-items-center"
+        >
+          <div>
+            <p class="pt-5 text-2xl text-center en-regular-font">
+              We need staff
+            </p>
+            <p class="pt-5 text-center jp-title-font">スタッフ募集中！</p>
+            <p class="pt-5 text-center jp-title-font">詳しくは下のボタンから</p>
+            <button>See More</button>
+          </div>
+          <div>
+            <p class="pt-5 text-2xl text-center en-regular-font">
+              Won't you be a speaker?
+            </p>
+            <p class="pt-5 text-center jp-title-font">登壇者募集中！</p>
+            <p class="pt-5 text-center jp-title-font">詳しくは下のボタンから</p>
+            <button>See More</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,4 +35,8 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.sub-content__bg {
+  background: rgba(255, 255, 255, 0.2);
+}
+</style>
--- components/Domains/TopContent/index.vue
+++ components/Domains/TopContent/index.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    <p>トップコンテンツです</p>
-    <div>
-      <p class="pt-5 en-title-font">aaaaa</p>
-      <p class="pt-5 en-regular-font">bbbb</p>
-      <p class="pt-5 jp-title-font">ああああ</p>
-      <p class="pt-5 jp-regular-font">日本語です</p>
+  <div
+    class="relative flex flex-col content-center justify-center w-full mt-32"
+  >
+    <div class="rectangle"></div>
+    <div class="flex content-center justify-center">
+      <h1 class="inline text-6xl en-title-font">PyCon JP 2021</h1>
+    </div>
+    <div class="flex content-center justify-center">
+      <p class="inline text-3xl en-title-font">
+        We Are Happy to See You Again!
+      </p>
+    </div>
+    <div class="flex content-center justify-center">
+      <div>----</div>
+    </div>
+    <div class="flex content-center justify-center">
+      <p class="pt-5 text-2xl en-regular-font">
+        Conference: Oct.15(Fri)- 16(Sat), 2021
+      </p>
+    </div>
+    <div class="flex content-center justify-center">
+      <p class="pt-5 text-sm jp-regular-font">
+        開催日時 : 2021年10月15日(金) - 16日(土)
+      </p>
+    </div>
+    <div class="flex content-center justify-center">
+      <p class="pt-5 text-sm jp-regular-font">SCROLL</p>
     </div>
   </div>
 </template>
@@ -14,4 +34,15 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.rectangle {
+  position: absolute;
+  width: 321px;
+  height: 321px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  background: rgba(255, 255, 255, 0.2);
+  transform: rotate(45deg);
+}
+</style>
