b8e0a6524eb1ef4233606c4b5658a0224ef0f032
k-nakamura <koya.nakamura@gmail.com>
2021-05-30 21:35:39 +0900

    #39 Contactの表記を追加

---
+++ components/Domains/BottomContent/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bottom__bg">
+    <section class="py-3">
+      <div class="flex justify-center contact__width">
+        <div class="block">
+          <h2 class="font-bold text-2xl mb-1">Contact</h2>
+          <p>ボランティアによる運営ですので、回答には1週間程度を要します。</p>
+          <p>何卒お待ちいただけますようにお願いします。</p>
+        </div>
+      </div>
+      <div class="flex justify-center mt-2">
+        <a
+          href="mailto:pyconjp@pycon.jp"
+          class="border text-white bg-auto border-white hover:text-black hover:bg-white font-bold py-2 px-8 rounded-full"
+        >
+          <span>pyconjp@pycon.jp</span>
+        </a>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.bottom__bg {
+  background: rgba(50, 50, 50, 0.7);
+}
+.contact__width {
+  max-width: 80%;
+  margin: 0 auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div class="text-white">
     <TopContent></TopContent>
     <SubContent></SubContent>
+    <BottomContent></BottomContent>
   </div>
 </template>
 
 <script>
 import TopContent from '~/components/Domains/TopContent'
 import SubContent from '~/components/Domains/SubContent'
+import BottomContent from '~/components/Domains/BottomContent'
 export default {
   components: {
     TopContent,
     SubContent,
+    BottomContent,
   },
 }
 </script>
