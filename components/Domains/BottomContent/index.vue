0c4db2350ff9699e78a7716c7f140373f4cc7de0
k-nakamura <koya.nakamura@gmail.com>
2021-06-01 23:06:41 +0900

    #39 Contactに英語表記を追加

--- components/Domains/BottomContent/index.vue
+++ components/Domains/BottomContent/index.vue
@@ -6,6 +6,8 @@
           <h2 class="font-bold text-2xl mb-1">Contact</h2>
           <p>ボランティアによる運営ですので、回答には1週間程度を要します。</p>
           <p>何卒お待ちいただけますようにお願いします。</p>
+          <p>It is run by volunteers. It takes about a week to answer.</p>
+          <p>Thank you for your patience.</p>
         </div>
       </div>
       <div class="flex justify-center mt-2">
