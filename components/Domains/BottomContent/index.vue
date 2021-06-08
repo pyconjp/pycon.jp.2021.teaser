b14cf33f0a5419654a24bf2ea28f87d94bdae62e
k-nakamura <koya.nakamura@gmail.com>
2021-06-09 01:20:00 +0900

    #39 デザイン修正

--- components/Domains/BottomContent/index.vue
+++ components/Domains/BottomContent/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="bottom__bg">
-    <section class="py-3">
-      <div class="flex justify-center contact__width">
-        <div class="block">
-          <h2 class="font-bold text-2xl mb-1">Contact</h2>
+  <div class="bottom-content py-24">
+    <div class="text-center">
+      <p class="text-2xl en-title-font">Contact</p>
+      <div class="mt-10">
+        <div class="text-sm jp-regular-font">
           <p>ボランティアによる運営ですので、回答には1週間程度を要します。</p>
           <p>何卒お待ちいただけますようにお願いします。</p>
+        </div>
+        <div class="text-sm en-regular-font mt-4">
           <p>It is run by volunteers. It takes about a week to answer.</p>
           <p>Thank you for your patience.</p>
         </div>
       </div>
-      <div class="flex justify-center mt-2">
+      <div class="flex justify-center contact-button-area">
         <a
           href="mailto:pyconjp@pycon.jp"
-          class="border text-white bg-auto border-white hover:text-black hover:bg-white font-bold py-2 px-8 rounded-full"
+          class="en-regular-font border text-white bg-auto border-white hover:text-black hover:bg-white rounded-full contact-button"
         >
           <span>pyconjp@pycon.jp</span>
         </a>
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -27,11 +29,17 @@ export default {}
 </script>
 
 <style scoped>
-.bottom__bg {
-  background: rgba(50, 50, 50, 0.7);
+.bottom-content {
+  height: 499px;
 }
-.contact__width {
-  max-width: 80%;
-  margin: 0 auto;
+.contact-button {
+  height: 50px;
+  width: 280px;
+  padding-top: 13px;
+  padding-bottom: 13px;
+}
+
+.contact-button-area {
+  margin-top: 30px;
 }
 </style>
